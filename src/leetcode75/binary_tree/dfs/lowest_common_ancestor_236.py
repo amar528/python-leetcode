60673fb916084a6b3be17ce3fa26d0fef74de953
amar528 <amar.om@icloud.com>
2024-05-30 10:45:56 +1000

    Leetcode 75

--- src/leetcode75/binary_tree/dfs/lowest_common_ancestor_236.py
+++ src/leetcode75/binary_tree/dfs/lowest_common_ancestor_236.py
@@ -5,18 +5,25 @@ class Solution:
 
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def dfs(node):
+
+            # base case, end recursive subtree
             if not node:
                 return None
 
+            # Either node can be the LCA of itself, also we want to end the
+            # recursion when we hit p or q
             if node == p or node == q:
                 return node
 
+            # Otherwise, we recurse for left and right subtrees
             left = dfs(node.left)
             right = dfs(node.right)
 
+            # If we have found both p and q, then our current node must be the closest ancestor
             if left and right:
                 return node
 
+            # Otherwise, we return any non-null result from the left or right subtrees
             return left or right
 
         return dfs(root)
