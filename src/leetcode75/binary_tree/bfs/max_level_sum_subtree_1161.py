7d8fd57998be50bb935747c80a77c1a1893f3212
amar528 <amar.om@icloud.com>
2024-06-13 09:59:02 +1000

    Tidy

--- src/leetcode75/binary_tree/bfs/max_level_sum_subtree_1161.py
+++ src/leetcode75/binary_tree/bfs/max_level_sum_subtree_1161.py
@@ -1,5 +1,6 @@
 import collections
 from typing import Optional
+
 from leetcode75.binary_tree.treenode import TreeNode
 
 
@@ -9,18 +10,28 @@ class Solution:
         if not root:
             return 0
 
+        # queue items are (level, node)
+        # enqueue the root
         queue = collections.deque([(1, root)])
+
+        # keep track of sums per level : level: sum
         level_sums = collections.defaultdict(int)
 
         while queue:
+
+            # deque the current level and node
             level, node = queue.popleft()
 
+            # add the node value to the total for this level (key)
             level_sums[level] += node.val
 
             if node.left:
+                # enqueue left child for next level
                 queue.append((level + 1, node.left))
 
             if node.right:
+                # enqueue right child for the next level
                 queue.append((level + 1, node.right))
 
+        # return the maximum sum of all levels
         return max(level_sums, key=level_sums.get)
