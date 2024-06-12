ed632aab3f0e5052b2dfd18319036b2bcb0d1b9f
amar528 <amar.om@icloud.com>
2024-06-13 08:23:08 +1000

    Tidy

--- src/leetcode75/binary_search/find_peak_element_162.py
+++ src/leetcode75/binary_search/find_peak_element_162.py
@@ -5,12 +5,17 @@ from typing import List
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
 
+        # element size
         n = len(nums)
 
+        # left and right indices
         left = 0
         right = n - 1
 
+        # loop until left and right cross
         while left <= right:
+
+            # the mid point
             mid = (left + right) // 2
 
             if mid > 0 and nums[mid] < nums[mid - 1]:
@@ -20,4 +25,5 @@ class Solution:
                 # go to right
                 left = mid + 1
             else:
+                # we have found the result
                 return mid
