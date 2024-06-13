a4507a837e99856abfe9dafc7ad5a9ba12883014
amar528 <amar.om@icloud.com>
2024-06-13 10:14:46 +1000

    Tidy

--- src/leetcode75/binary_tree/dfs/max_depth_104.py
+++ src/leetcode75/binary_tree/dfs/max_depth_104.py
@@ -5,7 +5,10 @@ from leetcode75.binary_tree.treenode import TreeNode
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        # base case: null node
         if not root:
             return 0
 
+        # return this level (1) plus the maximum of the left and right subtrees
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
