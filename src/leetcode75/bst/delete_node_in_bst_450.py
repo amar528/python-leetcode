0aa1a8e8f1a19f5027b81676f29bdc6ad1156f7d
amar528 <amar.om@icloud.com>
2024-06-13 10:32:13 +1000

    Tidy

--- src/leetcode75/bst/delete_node_in_bst_450.py
+++ src/leetcode75/bst/delete_node_in_bst_450.py
@@ -8,22 +8,22 @@ class Solution:
 
         def find(node, parent):
             if not node:
-                return None, parent
-
-            if node.val == key:
-                print(f'find returning node: {node} parent: {parent}')
                 return node, parent
 
-            if node.val < key:
+            if node.val == key:
+                return node, parent
+
+            if key < node.val:
                 return find(node.left, node)
             else:
                 return find(node.right, node)
 
         def delete(node, parent):
-            if node.left:
+            if node.left and node.right and node.left.val < node.right.val:
                 parent.left = node.left
+                parent.left.left = node.right
             else:
-                parent.right = node.right
+                parent.left = node.right
 
         # step 1 - find the node and its parent
         found_node, found_parent = find(root, None)
