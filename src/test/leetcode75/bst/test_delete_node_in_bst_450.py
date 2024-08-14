3eb3736aa221559e5cebfd0cb944408c75278950
amar528 <amar.om@icloud.com>
2024-08-15 09:02:33 +1000

    Delete node test

--- src/test/leetcode75/bst/test_delete_node_in_bst_450.py
+++ src/test/leetcode75/bst/test_delete_node_in_bst_450.py
@@ -5,28 +5,7 @@ from leetcode75.bst.delete_node_in_bst_450 import Solution
 
 
 class TestSolution(TestCase):
-    def test_delete_node_example1(self):
-        root = TreeNode(5,
-                        TreeNode(3,
-                                 TreeNode(2),
-                                 TreeNode(4)),
-                        TreeNode(6,
-                                 None,
-                                 TreeNode(7))
-                        )
 
-        key = 3
-        sol = Solution()
-
-        result = sol.deleteNode(root, key)
-        self.assertIsNotNone(result)
-
-        self.assertEqual(5, result.val)
-        self.assertEqual(2, result.left.val)
-        self.assertEqual(6, result.right.val)
-
-        self.assertIsNone(result.left.left)
-        self.assertEqual(4, result.left.right.val)
 
     def test_delete_node_example2(self):
         root = TreeNode(0)
